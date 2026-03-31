69a8374e0a257665391454c26b47fd86d209911c
FaizanMahammad <148524354+FaizanMahammad@users.noreply.github.com>
2026-03-31 22:00:33 +0530

    Create UpdateRegionRequestDto.cs

---
+++ Models/DTO/UpdateRegionRequestDto.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IndiaWalks.API.Models.DTO
+{
+    public class UpdateRegionRequestDto
+    {
+        //-------------With Model Validation also implemented in RegionsController-----------------------------
+        [Required]
+        [MinLength(3, ErrorMessage = "Code has to be a Minimum of 3 Characters")]
+        [MaxLength(3, ErrorMessage = "Code has to be a Maximum of 3 Characters")]
+        public string Code { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Name should not be more than 100 Characters")]
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+        /*--------------Without Model Validation-------------
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+        */
+    }
+}
