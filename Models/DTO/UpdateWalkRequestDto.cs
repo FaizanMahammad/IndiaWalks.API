d362a14d41643ffb3dbd68a084c94cbfe808a7f4
FaizanMahammad <148524354+FaizanMahammad@users.noreply.github.com>
2026-03-31 22:01:06 +0530

    Create UpdateWalkRequestDto.cs

---
+++ Models/DTO/UpdateWalkRequestDto.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IndiaWalks.API.Models.DTO
+{
+    public class UpdateWalkRequestDto
+    {
+        //-------------With Model Validation also implemented in RegionsController-----------------------------
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+        [Required]
+        [Range(0, 50)]
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+        [Required]
+        public Guid DifficultyId { get; set; }
+        [Required]
+        public Guid RegionId { get; set; }
+
+        /*--------------Without Model Validation-------------
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+        */
+    }
+}
